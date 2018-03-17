9da26e5f13a5a8392aff9e16b2cef310e420217f
Liang <lyodev@gmail.com>
2018-03-16 23:13:22 -0700

    commit menu navigation demo

---
+++ Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuNavigation : MonoBehaviour 
+{
+	public GameObject popup;
+	public List<Button> buttons = new List<Button>();
+
+	private int[,] btnPos =
+	{
+		{0, 0},
+		{0, 1},
+		{0, 2},
+
+		{1, 1},
+		{1, 0},
+		{1, 2},
+
+		{2, 1},
+		{2, 2},
+		{2, 0},
+
+		{4, 1},
+		{4, 2},
+		{4, 0},
+
+		{3, 1},
+		{3, 2},
+		{3, 0},
+
+		{5, 1},
+		{5, 2},
+		{5, 0},
+	};
+
+	private int selectedIndex = 0;
+	private Vector3 prevVector = Vector3.zero;
+	private float currentCooldown = 0;
+	private float swipeCooldown = 0.02f;
+
+	void Update () 
+	{
+		int buttonCount = buttons.Count;
+		for(int i = 0; i < buttonCount; ++i)
+		{
+			if (i == selectedIndex)
+			{
+				buttons[i].Select();
+			}
+		}
+
+		if (TalonConnect.TalonRing == null)
+			return;
+
+		TalonSDK.TalonRing ring = TalonConnect.TalonRing;
+
+		if (ring.BottomButtonDown)
+		{
+			popup.SetActive(!popup.activeSelf);
+		}
+
+		if (popup.activeSelf)
+			return;
+		
+		Vector3 forward = ring.Orientation * Vector3.forward;
+
+		if (prevVector != Vector3.zero && !ring.Recentered)
+		{
+			if (currentCooldown > 0)
+			{
+				currentCooldown -= Time.deltaTime;
+			}
+			else
+			{
+				Vector3 speed = (forward - prevVector) / Time.deltaTime;
+
+				float Xmag = Mathf.Abs(speed.x);
+				float Ymag = Mathf.Abs(speed.y);
+
+				if (Xmag > 2.5 || Ymag > 2.5) 
+				{
+					currentCooldown = swipeCooldown;
+					if (Xmag > Ymag) {
+						if (speed.x < 0) {
+							OnLeft();
+						} else {
+							OnRight();
+						}
+					} else {
+						if (speed.z < 0) {
+							OnUp();
+						} else {
+							OnDown();
+						}
+					}
+				}
+			}
+		}
+
+		prevVector = forward;
+	}
+
+	private void OnLeft()
+	{
+		Debug.Log("OnLeft");
+		int x = btnPos[selectedIndex, 0];
+		int y = btnPos[selectedIndex, 1];
+		if (x > 0) x--;
+		int idx = getIndex(x, y);
+		if (idx != -1) selectedIndex = idx;
+	}
+
+	private void OnRight()
+	{
+		Debug.Log("OnRight");
+		int x = btnPos[selectedIndex, 0];
+		int y = btnPos[selectedIndex, 1];
+		if (x < 5) x++;
+		int idx = getIndex(x, y);
+		if (idx != -1) selectedIndex = idx;
+	}
+
+	private void OnUp()
+	{
+		Debug.Log("OnUp");
+		int x = btnPos[selectedIndex, 0];
+		int y = btnPos[selectedIndex, 1];
+		if (y > 0) y--;
+		int idx = getIndex(x, y);
+		if (idx != -1) selectedIndex = idx;
+	}
+
+	private void OnDown()
+	{
+		Debug.Log("OnDown");
+		int x = btnPos[selectedIndex, 0];
+		int y = btnPos[selectedIndex, 1];
+		if (y < 2) y++;
+		int idx = getIndex(x, y);
+		if (idx != -1) selectedIndex = idx;
+	}
+
+	private int getIndex(int x, int y)
+	{
+		int buttonCount = buttons.Count;
+		for(int i = 0; i < buttonCount; ++i)
+		{
+			if (x == btnPos[i, 0] && y == btnPos[i, 1])
+				return i;
+		}
+		return -1;
+	}
+}
---
+++ Assets/Scripts/TalonConnect.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalonConnect : MonoBehaviour 
+{    
+    public Text logText;
+    private static TalonConnect _instance;
+    private bool connecting;
+    private TalonSDK.TalonRing talonRing;
+
+    void Awake()
+    {
+        if (_instance != null)
+        {
+            Debug.LogError("More than one TalonConnect instance was found in your scene");
+            enabled = false;
+            return;
+        }
+        _instance = this;
+        connecting = false;
+        talonRing = null;
+    }
+
+    void log(string log)
+    {
+        if (logText != null)
+            logText.text = log;
+    }
+
+    void Start()
+    {
+        log("Scanning...");
+        StartCoroutine(Scanning());
+    }
+
+    IEnumerator Scanning()
+    {
+        TalonSDK.Talon.Scan();
+        yield return new WaitForSeconds(10);
+        if (talonRing == null)
+        {
+            StartCoroutine(Scanning());
+        }
+    }
+
+    public void OnDiscovered(string deviceId)
+    {
+        log("OnDiscovered: " + deviceId);
+        if(!connecting && talonRing == null)
+        {
+            connecting = true;
+            TalonSDK.Talon.Connect(deviceId);
+        }
+    }
+
+    public void OnConnected(string deviceId)
+    {
+        log("OnConnected: " + deviceId);
+        talonRing = TalonSDK.Talon.GetConnectedRing(deviceId);
+        connecting = false;
+    }
+
+    public void OnDisconnected(string deviceId)
+    {
+        log("OnDisconnected: " + deviceId);
+        talonRing = null;
+        connecting = false;
+        TalonSDK.Talon.Scan(); //retry?
+    }
+
+    public static TalonSDK.TalonRing TalonRing
+    {
+        get 
+        {
+            return _instance == null ? null : _instance.talonRing;
+        }
+    }
+}
